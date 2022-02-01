10c9f2d5edcc411aa5df413b161e1f017e72fbb8
Juan Menchero <juanbmamigo@gmail.com>
2022-02-01 20:15:23 +0100

    feature: add word count view

--- front/store/analysis.js
+++ front/store/analysis.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export const state = () => ({
   json: undefined
 })
@@ -42,5 +44,27 @@ export const getters = {
       )
     }
     return emojis
+  },
+  words (state, getters) {
+    const words = {}
+    if (state.json) {
+      state.json.chats.list.map(
+        chat => {
+          chat.messages.map(message => {
+            if (message.from_id === getters.user && message.text.match) {
+              const message_words = message.text.match(/\b(\w+)\b/g)
+              if (message_words) {
+                message_words.map(word => {
+                  if (word.length > 4) {
+                    words[word] = words[word] ? words[word] + 1 : 1
+                  }
+                })
+              }
+            }
+          })
+        }
+      )
+    }
+    return _.pickBy(words, word => word > 100)
   }
 }
