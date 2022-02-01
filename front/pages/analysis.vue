117ed82e4db56e5f99656bec5fdc1f82287279f3
Juan Menchero <juanbmamigo@gmail.com>
2022-02-01 18:56:12 +0100

    feature: add emojis wall first approach

--- front/pages/analysis.vue
+++ front/pages/analysis.vue
@@ -1,8 +1,14 @@
 <template>
   <full-page ref="fullpage">
     <section class="section section--emojis">
-      <h1>Emojis</h1>
-      {{ emojis }}
+      <div 
+        v-for="(count, emoji) in emojis"
+        :key="emoji"
+        :style="emojiStyle(count)"
+      >
+        {{ emoji }}
+      </div>
+      <h1>Your emojis wall</h1>
     </section>
   </full-page>
 </template>
@@ -15,15 +21,36 @@ export default {
 
   computed: {
     ...mapGetters('analysis', ['emojis'])
+  },
+
+  methods: {
+    emojiStyle(count) {
+      return `
+        position: absolute;
+        font-size: ${count}px;
+        left: ${Math.floor(Math.random()*100)}vw;
+        top: ${Math.floor(Math.random()*100)}vh;
+        transform: translatey(0px);
+        animation: float ${Math.random()*10+5}s ease-in-out;
+      `
+    }
   }
 }
 </script>
 
-<style scoped>
+<style>
 .section--emojis {
   background: linear-gradient(#001122,#000000);
   display: flex;
   justify-content: center;
-  flex-wrap: wrap;
+}
+
+@keyframes float {
+	0% {
+		transform: translatey(100vh);
+	}
+	100% {
+		transform: translatey(0vh);
+	}
 }
 </style>
