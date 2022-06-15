b4f5a24110b9eb86b0f719cd28ef0f0c81878191
Juan Menchero <juanbmamigo@gmail.com>
2022-06-15 20:36:14 +0200

    Refactor action button

---
+++ front/components/atoms/ActionButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-button
+    class="action-button"
+    type="is-primary"
+    rounded
+    @click="handleClick"
+  >{{ text }}</b-button>
+</template>
+
+<script>
+export default {
+  name: 'ActionButton',
+  props: ['text'],
+  methods: {
+    handleClick () {
+      this.$emit('action-clicked')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.action-button {
+  margin: 1rem;
+  font-size: 1vw;
+  float: right;
+}
+</style>
+
+
+
--- front/components/intro/ProjectIntroduction.vue
+++ front/components/intro/ProjectIntroduction.vue
@@ -4,20 +4,23 @@
       <h1>Want to check what do they know about you?</h1>
       <p>We all leave information about ourselves on the internet, with every google search and every message or email we send.</p>
       <p>But which kind of insights do big tech companies have available about us?</p>
-      <b-button
-        class="intro__button"
-        type="is-primary"
-        rounded
-        @click="$emit('click-in-show')"
-      >Show me my insights!</b-button>
+      <action-button
+        text="Show me my insights!"
+        @action-clicked="$emit('click-in-show')"
+      />
     </div>
     <img class="intro__video" src="~/assets/summary.gif"/>
   </section>
 </template>
 
 <script>
+import ActionButton from '../atoms/ActionButton.vue'
+
 export default {
-  name: 'ProjectIntroduction'
+  name: 'ProjectIntroduction',
+  components: {
+    ActionButton
+  },
 }
 </script>
 
@@ -37,10 +40,5 @@ export default {
   min-width: 200px;
   width: 20%;
   margin: 2rem;
-}
-.intro__button {
-  margin: 1rem;
-  font-size: 1vw;
-  float: right;
 }
 </style>
