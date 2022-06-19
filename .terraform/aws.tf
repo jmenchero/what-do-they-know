5cb46fd67ee8e349d4382cbdb7a884d539002abe
Juan Menchero <juanbmamigo@gmail.com>
2022-06-19 20:05:24 +0200

    Add cloudfront

--- .terraform/aws.tf
+++ .terraform/aws.tf
@@ -32,3 +32,41 @@ resource "aws_redshift_cluster" "db" {
   cluster_type        = "single-node"
   skip_final_snapshot = true
 }
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.front.bucket_regional_domain_name
+    origin_id = "wdtk-front.s3-website.eu-west-3.amazonaws.com"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "wdtk-cdn"
+  default_root_object = "index.html"
+  default_cache_behavior {
+    target_origin_id = "wdtk-front.s3-website.eu-west-3.amazonaws.com"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "ES"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
